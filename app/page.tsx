2584663bf76f0474b843614e45a4745d148d6af2
Hyunjung <hazel.s.suh@gmail.com>
2025-05-12 19:35:08 +0900

    Edit WORK_EXPERIENCE layout

--- app/page.tsx
+++ app/page.tsx
@@ -206,18 +206,20 @@ export default function Personal() {
                 size={64}
               />
               <div className="relative h-full w-full rounded-[15px] bg-white p-4 dark:bg-zinc-950">
-                <div className="relative flex w-full flex-row justify-between">
-                  <div>
-                    <h4 className="font-normal dark:text-zinc-100">
-                      {job.title}
-                    </h4>
+                <div className="relative flex w-full">
+                  <div className="flex w-full flex-col">
+                    <div className="relative flex w-full items-center justify-between">
+                      <h4 className="font-normal dark:text-zinc-100">
+                        {job.title}
+                      </h4>
+                      <p className="text-zinc-600 dark:text-zinc-400">
+                        {job.start} - {job.end}
+                      </p>
+                    </div>
                     <p className="text-sm text-zinc-500 dark:text-zinc-400">
                       {job.company}
                     </p>
                   </div>
-                  <p className="text-zinc-600 dark:text-zinc-400">
-                    {job.start} - {job.end}
-                  </p>
                 </div>
               </div>
             </a>
