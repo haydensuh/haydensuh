fb12c709300a1796c4edc8ff9cee0e00a728d9b0
Hyunjung <hazel.s.suh@gmail.com>
2025-05-18 02:11:23 +0900

    Refactor blog

--- app/[locale]/blog/[slug]/page.tsx
+++ app/[locale]/blog/[slug]/page.tsx
@@ -1,14 +1,11 @@
 import { notFound } from 'next/navigation'
 import { getPostBySlug } from '@/lib/posts'
 
-type PageProps = {
-  params: {
-    locale: string
-    slug: string
-  }
-}
-
-export default async function BlogPostPage({ params }: PageProps) {
+export default async function BlogPostPage({
+  params,
+}: {
+  params: { locale: string; slug: string }
+}) {
   const { locale, slug } = params
   const post = await getPostBySlug(locale, slug)
 
