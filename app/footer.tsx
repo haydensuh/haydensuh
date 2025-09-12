c613280de7a8aca09dc88d828d191130040afc7f
Hyunjung Suh <suhhj@kodebox.io>
2025-09-12 11:29:46 +0900

    Remove footer loop

--- app/footer.tsx
+++ app/footer.tsx
@@ -94,11 +94,7 @@ export function Footer() {
   return (
     <footer className="mt-24 border-t border-zinc-100 px-0 py-4 dark:border-zinc-800">
       <div className="flex items-center justify-between">
-        <TextLoop className="text-xs text-zinc-500">
-          <span>© 2025 Hayden Suh.</span>
-          <span>Built with Motion-Primitives.</span>
-        </TextLoop>
-
+          <p className="text-xs text-zinc-500">© 2025 Hayden Suh.</p>
         <div className="flex items-center text-xs text-zinc-400">
           <LocaleSwitch />
           <ThemeSwitch />
