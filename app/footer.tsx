73a84d99f3639a13af1d6ebabcdd99f48f6a6c40
Hyunjung Suh <suhhj@kodebox.io>
2025-10-29 12:15:09 +0900

    Delete textloop

--- app/footer.tsx
+++ app/footer.tsx
@@ -1,6 +1,5 @@
 'use client'
 import { AnimatedBackground } from '@/components/ui/animated-background'
-import { TextLoop } from '@/components/ui/text-loop'
 import { MonitorIcon, MoonIcon, SunIcon } from 'lucide-react'
 import { useTheme } from 'next-themes'
 import { useRouter } from 'next/navigation'
