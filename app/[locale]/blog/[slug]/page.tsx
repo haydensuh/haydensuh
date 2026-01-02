488ed4ac82fab579118bf5a98eaa24ffcdaa0793
Hyunjung Suh <suhhj@kodebox.io>
2026-01-02 17:21:37 +0900

    라우트 에러 수정

--- app/[locale]/blog/[slug]/page.tsx
+++ app/[locale]/blog/[slug]/page.tsx
@@ -1,7 +1,7 @@
 import { ClientWrapper } from './ClientBlogPage'
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export default function BlogPage({ params }: any) {
-  const { locale, slug } = params
+export default async function BlogPage({ params }: any) {
+  const { locale, slug } = await params
   return <ClientWrapper locale={locale} slug={slug} />
 }
--- app/[locale]/works/[slug]/page.tsx
+++ app/[locale]/works/[slug]/page.tsx
@@ -2,7 +2,7 @@
 import { ClientWorkDetailPage } from './ClientWorkDetailPage'
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export default function WorkDetailPage({ params }: { params: any }) {
-  const { locale, slug } = params
+export default async function WorkDetailPage({ params }: { params: any }) {
+  const { locale, slug } = await params
   return <ClientWorkDetailPage locale={locale} slug={slug} />
 }
